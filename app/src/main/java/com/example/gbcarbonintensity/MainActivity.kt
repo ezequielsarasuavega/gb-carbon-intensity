a59616bde59686c65d217fc7687fe7d25fda7076
Ezekiel <ezequiel.sarasua@bnext.es>
2021-04-01 12:39:43 +0200

    add main activity

---
+++ app/src/main/java/com/example/gbcarbonintensity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gbcarbonintensity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Main activity for the GB Carbon Intensity app. Holds the Navigation Host Fragment
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
